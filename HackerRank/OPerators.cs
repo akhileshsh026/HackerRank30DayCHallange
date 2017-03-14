3f233c8a23ec4533c92b147dfa47e6f776488d33
Akhileshsh026@hotmail.com <Akhilesh@123>
2017-03-15 00:12:08 +0530

    Add project files.

---
+++ HackerRank/OPerators.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace HackerRank
+{
+    class OPerators
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the MealCost");
+
+            double mealCost = double.Parse(Console.ReadLine());
+            int tipPercent = int.Parse(Console.ReadLine());
+            int taxPercent = int.Parse(Console.ReadLine());
+
+            // calulation Part
+             
+            double tip = mealCost * ((double)tipPercent / 100); //division by 100 always return 0.
+            double tax = mealCost * ((double)taxPercent / 100);
+            double totalCost = mealCost + tip + tax;
+
+            Console.WriteLine(Math.Round(totalCost));
+            Console.ReadLine();
+
+        }
+    }
+}
