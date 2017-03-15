9aa910fe1f2f2475b9b38ae44af370f221b1df8c
Akhileshsh026@hotmail.com <Akhilesh@123>
2017-03-15 09:43:50 +0530

    Completed Solution in C#

---
+++ ConditionalOperator/ConditionalOperatorProgram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalOperator
+{
+    class ConditionalOperatorProgram
+    {
+        static void Main()
+        {
+           
+
+       int n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0) // checking weather the interger is even or odd
+            {
+
+                if (n > 2 && n < 5)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else if (n > 6 && n <= 20)
+                {
+                    Console.WriteLine("Weird");
+                }
+                else if (n > 20)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+
+            }
+            else Console.WriteLine("Weird");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
