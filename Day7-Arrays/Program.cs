1ec31158a0d43dc7989c5e2871a0f3b512b5f3cb
Akhilesh Kumar <akhileshsh026@hotmail.com>
2017-04-22 19:34:13 +0530

    Completed challange 9 with recursion .

--- Day7-Arrays/Program.cs
+++ Day7-Arrays/Program.cs
@@ -10,22 +10,32 @@ namespace Day7_Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Number");
-            int N = Convert.ToInt32(Console.ReadLine());
-            int[] A = new int[N];
-            string[] s = new string[N]; 
-            for (int i = 0; i < N; i++)
-            {
-                
-                    s[i] = Console.ReadLine();
-                
+            //Console.WriteLine("Enter the Number");
+            //int N = Convert.ToInt32(Console.ReadLine());
+            //int[] A = new int[N];
+            //string[] s = new string[N]; 
+            //for (int i = 0; i < N; i++)
+            //{
 
+            //        s[i] = Console.ReadLine();
+
+
+            //}
+
+            //for (int i = N-1; i >=0 ; i--)
+            //{
+            //    Console.Write(A[i]);
+            //}
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            foreach (var item in arr)
+            {
+                Console.Write(item);
             }
 
-            for (int i = N-1; i >=0 ; i--)
-            {
-                Console.Write(A[i]);
-            }
+
 
             Console.ReadKey();
         }
---
+++ Day9Recursion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int fact=  factorial(N);
+
+            Console.WriteLine(fact);
+
+
+        }
+
+        public static int factorial(int N )
+        {       // base case : where our algorithum will stop .
+            if (N <= 1)
+                return 1;    // multiplicative identity .
+            else     // recursive case : where our algorithum will repeted again and again .
+                return N * factorial(N - 1);
+        }
+    }
+}
